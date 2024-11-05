fa4e18cb55f69a694bad4e0fc0fe3cad5cb0cbd5
Kab-35-16 <Kab-35-16@KAB-31-5->
2024-11-05 11:14:14 +0200

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Задача 2. Изменить размер одномерного массива.
+
+            //int[] mass = new int[5] { 1, 2, 3, 4, 5 };
+            //foreach (int i in mass) { Console.WriteLine(i); }
+            //Console.WriteLine("--------------");
+            //Array.Resize(ref mass, 4);
+            //foreach (int i in mass) { Console.WriteLine(i); }
+            //Console.WriteLine("--------------");
+            //Array.Resize(ref mass, 3);
+            //foreach (int i in mass) { Console.WriteLine(i); }
+            //Console.WriteLine("--------------");
+            //Array.Resize(ref mass, 2);
+            //foreach (int i in mass) { Console.WriteLine(i); }
+            //Console.WriteLine("--------------");
+            //Array.Resize(ref mass, 6);
+            //foreach (int i in mass) { Console.WriteLine(i); }
+            //Console.Read();
+
+            //Задача 3. Найти индекс первого отрицательного элемента одномерного массива, заполненного случайным образом в диапазоне от -5 до 5.
+
+            int[] nums = new int[5];
+            Random rand = new Random();
+            for (int i=0;i<5;i++) {nums[i] = rand.Next(-5,5); Console.WriteLine(nums[i]); }
+                int mySecondIndex = Array.FindIndex(nums, min => min<0);
+            Console.WriteLine($"Индекс первого отрицательного элемента - {mySecondIndex}");
+            Console.Read();
+        }
+    }
+}
